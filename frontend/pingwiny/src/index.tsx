0bcf8ec2e913385830e6138998132446a4c80169
Dawid Liberda <dave.liberda@yahoo.com>
2023-04-01 15:24:55 +0200

    Added commands to build and run react app to GitHub actions workflow (#4)
    * Added commands to build and run react app to GitHub actions workflow
    
    * Fixed error with changing directory in Github Actions workflow
    
    * Fixed error about package.json file not found
    
    * Added Azure Web App Service to the Github Actions workflow
    
    * Fixed error about incorrectly set working directory
    
    * Fixed error about unknown build job
    
    * Fixed error about using old Node.js version
    
    * Fixed error about unknow package-lock.json file from Node.js
    
    * Added temporary listing of directiories
    
    * Fixed error about not resolved dependencies
    
    * Added force to installation of npm modules
    
    * Fixed error about unterminated JSX contents in src/index.tsx file
    
    * Fixed path to upload zip archive with binary of react app
    
    * Removing zip and unzip commands from the workflow

--- frontend/pingwiny/src/index.tsx
+++ frontend/pingwiny/src/index.tsx
@@ -34,8 +34,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
 
-      <Provider store={store}>
-        <App/>
+      <Provider store={store} children={<App/>}/>  
 
   </React.StrictMode>
 );
